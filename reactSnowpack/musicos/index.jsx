5bc94e1dd1f60259cd0b60f85805d609669ac005
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2021-02-22 11:56:55 +0100

    snowpack react essai ok

---
+++ reactSnowpack/musicos/index.jsx
@@ -0,0 +1,6 @@
+/* Add JavaScript code here! */
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<div>"HELLO REACT"</div>, document.getElementById('root'));
+
+//console.log('Hello World! You did! Welcome to Snowpack :D');
