3a3321e617900eaa9324521e64b907a86c12bff7
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2021-02-22 23:55:43 +0100

    color problem solved. now need to fold back on click when screen large

--- essaiVue3ViteElementPlus/musicos/src/components/Navbar.vue
+++ essaiVue3ViteElementPlus/musicos/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar">
     <ul class="navbar-nav">
       <li class="logo">
-        <a href="#" class="nav-link">
+        <a href="#" class="nav-link" id="title">
           <span class="link-text logo-text">Musicos</span>
           <svg
             aria-hidden="true"
@@ -16,65 +16,29 @@
           >
             <g class="fa-group">
               <path
-                fill="currentColor"
+                fill='#f5e5b8'
                 d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
-                class="fa-secondary"
               ></path>
               <path
-                fill="currentColor"
+                fill='#f89d15'
                 d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
-                class="fa-primary"
               ></path>
             </g>
           </svg>
         </a>
       </li>
 
-      <li class="nav-item" @click="openTab('Home')">
+      <li v-for="tab in myTabs" :key="tab.id" class="nav-item"
+          v-bind:class="{ 'isSelected': indexActive === tab.id }" 
+          @click="openTab(tab)">
         <a href="#" class="nav-link">
           <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
             <path
-              fill="currentColor"
-              d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z"
+              fill='#9e9e9e'
+              v-bind:d="tab.svgString"
             />
           </svg>
-          <span class="link-text">Home</span>
-        </a>
-      </li>
-
-      <li class="nav-item" @click="openTab('Search')">
-        <a href="#" class="nav-link">
-          <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
-            />
-          </svg>
-          <span class="link-text">Search</span>
-        </a>
-      </li>
-
-      <li class="nav-item" @click="openTab('AddSong')">
-        <a href="#" class="nav-link">
-          <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z"
-            />
-          </svg>
-          <span class="link-text">Add song</span>
-        </a>
-      </li>
-
-      <li class="nav-item" @click="openTab('Settings')">
-        <a href="#" class="nav-link">
-          <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"
-            />
-          </svg>
-          <span class="link-text">Settings</span>
+          <span class="link-text">{{tab.text}}</span>
         </a>
       </li>
     </ul>
@@ -86,6 +50,33 @@ export default {
   data() {
     return {
       isCollapse: false,
+      indexActive: 0,
+      myTabs: [
+        {
+          id: 0,
+          text: 'Home',
+          routeName: 'Home',
+          svgString: "M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z",
+        },
+        {
+          id: 1,
+          text: 'Search',
+          routeName: 'Search',
+          svgString: "M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z",
+        },
+        {
+          id: 2,
+          text: 'Add song',
+          routeName: 'AddSong',
+          svgString: "M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z",
+        },
+        {
+          id: 3,
+          text: 'Settings',
+          routeName: 'Settings',
+          svgString: "M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z",
+        },
+      ]
     };
   },
   methods: {
@@ -95,8 +86,9 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
-    openTab(routeName){
-      this.$router.push({name:routeName})
+    openTab(tab){
+      this.indexActive = tab.id
+      this.$router.push({name: tab.routeName})
     }
   },
 };
@@ -106,11 +98,23 @@ export default {
 :root {
   font-size: 16px;
   font-family: "Open Sans";
-  --text-primary: #b6b6b6;
+  --text-primary: #9e9e9e;
   --text-secondary: #ececec;
   --bg-primary: #23232e;
   --bg-secondary: #141418;
   --transition-speed: 600ms;
+}
+
+.isSelected{
+  filter: grayscale(0%) opacity(1);
+  background: #000000;
+  /* var(--bg-secondary); */
+  /* color: var(--text-secondary); */
+  color: #1900ff;
+}
+
+.isSelected svg path{
+  fill: white;
 }
 
 .navbar {
@@ -135,48 +139,36 @@ export default {
   width: 100%;
 }
 
-/* .nav-item:last-child {
-  margin-top: auto;
-} */
-
 .nav-link {
   display: flex;
   align-items: center;
   height: 5rem;
   color: var(--text-primary);
   text-decoration: none;
-  /* filter: grayscale(100%) opacity(0.7); */
-  transition: var(--transition-speed);
+  filter: grayscale(0%) opacity(1);
+  /* transition: var(--transition-speed); */
 }
 
-.nav-link:hover {
+#title {
+  /* filter: grayscale(0%) opacity(1); */
+}
+
+/* .nav-link:hover {
   filter: grayscale(0%) opacity(1);
   background: var(--bg-secondary);
   color: var(--text-secondary);
-}
+} */
 
 .link-text {
   display: none;
   margin-left: 1rem;
+  color: #e4e4e4;
 }
 
 .nav-link svg {
   width: 2rem;
   min-width: 2rem;
   margin: 0 1.5rem;
-}
-
-.fa-primary {
-  color: #ff7eee;
-}
-
-.fa-secondary {
-  color: #df49a6;
-}
-
-.fa-primary,
-.fa-secondary {
-  transition: var(--transition-speed);
 }
 
 .logo {
@@ -189,7 +181,7 @@ export default {
   font-size: 1.5rem;
   letter-spacing: 0.3ch;
   width: 100%;
-  filter: grayscale(0%) opacity(1);
+  /* filter: grayscale(0%) opacity(1); */
 }
 
 .logo svg {
@@ -202,6 +194,7 @@ export default {
   position: absolute;
   left: -999px;
   transition: var(--transition-speed);
+  color: #ffc62b;
 }
 
 .navbar:hover .logo svg {
