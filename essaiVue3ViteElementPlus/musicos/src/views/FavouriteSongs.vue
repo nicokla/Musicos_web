7d1b75d13490084d74064acab8fd2479cb3aa84a
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2021-02-23 17:19:15 +0100

    debut favourites (todo remove delete buttons)

--- essaiVue3ViteElementPlus/musicos/src/views/FavouriteSongs.vue
+++ essaiVue3ViteElementPlus/musicos/src/views/FavouriteSongs.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="FavouriteSongs">
+  <div id="FavouriteSongs">
     <h1 class="subheading grey--text">Favourite Songs</h1>
+    <div class="listOfSongs">
+      <SongCell v-for="song in songs" :theSong="song" @deleteEvent="deleteSong(song.id)"/>
+    </div>
   </div>
 </template>
 
 
 <script>
-import {db, firebase, getCurrentUser} from '../firebase/db'
+import {db, getCurrentUser} from '../firebase/db'
+import SongCell from '../components/SongCell.vue'
 
 export default {
+  components: { SongCell },
   data() {
     return {
       songs: []
@@ -20,16 +25,27 @@ export default {
 		},
 		deleteSong(id){
 			db.collection('songs').doc(id).delete()
+      this.songs = this.songs.filter(x => {
+        return x.id != id;
+      })
+      // TODO : do it locally conditionnaly on delete() being successfull
 		}
   },
   async mounted() {
     // console.log('userID', isLoggedIn().uid)
     // TypeError: Cannot read property 'uid' of null
-    const answer = await db.collection("songs").where("ownerID", "==", getCurrentUser().uid).get();
+    //  
+    const answer = await db.collection("users")
+                           .doc(getCurrentUser().uid)
+                           .collection('likedSongs')
+                           .get();
     // console.log(answer)
     this.songs=[]
     answer.forEach((doc)=>{
-      this.songs.push({id: doc.id, title: doc.data().title})//.data().name})
+      this.songs.push({
+        id: doc.id,
+        ...doc.data()
+      })//.data().name})
     })
     // this.songs = response.data
   },
@@ -45,5 +61,24 @@ export default {
 <style scoped>
 .content h1:not(:first-child) {
   margin-top: 3px;
+}
+
+#FavouriteSongs{
+  padding: 4mm;
+  --tw-bg-opacity: 1;
+  background-color: rgba(254, 243, 199, var(--tw-bg-opacity));	
+}
+
+.listOfSongs {
+  padding-top: 2mm;
+  padding-bottom: 2mm;
+
+	display: flex;
+  flex-direction:column;
+  flex-wrap:nowrap;
+	/* flex-flow: row wrap; */
+	justify-content:flex-start;
+  align-content:center;
+	align-items:center;
 }
 </style>
