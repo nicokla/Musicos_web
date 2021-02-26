8d9380640df9afebd98f7f0364230a52e282d65e
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2021-02-26 19:12:40 +0100

    followed users and user page ok. todo: song page.

--- essaiVue3ViteElementPlus/musicos/src/views/FollowedUsers.vue
+++ essaiVue3ViteElementPlus/musicos/src/views/FollowedUsers.vue
@@ -1,27 +1,66 @@
 <template>
-  <div class="FollowedUsers paddedContainer">
-    <h1 class="subheading grey--text">Followed users</h1>
+  <div id="FollowedUsers" class="paddedContainer">
+    <h1 class="subheading grey--text">Followed Users</h1>
+    <div class="listOfUsers">
+      <UserCell v-for="user in users" :theUser="user" 
+          @click="openUser(user)"/>
+    </div>
   </div>
 </template>
 
 
 <script>
+import {db, getCurrentUser} from '../firebase/db'
+import UserCell from '../components/UserCell.vue'
 
 export default {
+  components: { UserCell },
   data() {
     return {
-      projects: []
+      users: []
     }
   },
-  async mounted(){
-	},
   methods:{
-  }
+    openUser(user){
+      this.$router.push({name: 'User', params: {id: user.id} })
+    }
+  },
+  async mounted() {
+    const answer = await db.collection("users")
+                           .doc(getCurrentUser().uid)
+                           .collection('followedUsers')
+                           .get();
+    this.users=[]
+    answer.forEach((doc)=>{
+      this.users.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
+  },
 }
 </script>
 
 <style scoped>
 .content h1:not(:first-child) {
   margin-top: 3px;
+}
+
+#FollowedUsers{
+  --tw-bg-opacity: 1;
+  background-color: rgba(254, 243, 199, var(--tw-bg-opacity));	
+}
+
+.listOfUsers {
+  padding-top: 2mm;
+  padding-bottom: 2mm;
+
+	display: flex;
+  flex-direction:column;
+  flex-wrap:nowrap;
+	/* flex-flow: row wrap; */
+	justify-content:flex-start;
+  align-items:stretch;
+  align-content:center;
 }
 </style>
--- essaiVue3ViteElementPlus/musicos/src/views/User.vue
+++ essaiVue3ViteElementPlus/musicos/src/views/User.vue
@@ -1,25 +1,66 @@
 <template>
-  <div class="User paddedContainer">
-    <h1 class="subheading grey--text">User</h1>
-    <p>{{ $route.params.id }}</p>
+  <div id="User" class="paddedContainer">
+    <h1 class="subheading grey--text">User songs</h1>
+    <div class="listOfSongs">
+      <SongCell v-for="song in songs" :theSong="song"
+          @click="openSong(song)"/>
+    </div>
   </div>
 </template>
 
 
 <script>
+import {db, getCurrentUser} from '../firebase/db'
+import SongCell from '../components/SongCell.vue'
+
 export default {
   data() {
     return {
-      projects: []
+      userId: this.$route.params.id,
+      songs: []
     }
   },
+
   async mounted(){
+    const answer = await db.collection("songs").where("ownerID", "==", this.userId).get();
+    this.songs=[]
+    answer.forEach((doc)=>{
+      this.songs.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
 	},
+  
   methods:{
+    openSong(song){
+      this.$router.push({name: 'Song', params: {id: song.id} })
+    }
   }
 }
 </script>
 
 <style scoped>
 
+.content h1:not(:first-child) {
+  margin-top: 3px;
+}
+
+#User{
+  --tw-bg-opacity: 1;
+  background-color: rgba(254, 243, 199, var(--tw-bg-opacity));	
+}
+
+.listOfSongs {
+  padding-top: 2mm;
+  padding-bottom: 2mm;
+
+	display: flex;
+  flex-direction:column;
+  flex-wrap:nowrap;
+	/* flex-flow: row wrap; */
+	justify-content:flex-start;
+  align-items:stretch;
+  align-content:center;
+}
 </style>
