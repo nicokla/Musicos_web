d272af13328c579f5614f582335541cf9d723277
Nicolas Klarsfeld <nicolas.klarsfeld@gmail.com>
2021-02-28 15:25:44 +0100

    keeping only vue 3 directory for production

--- reactSnowpack/musicos/index.jsx
+++
@@ -1,6 +0,0 @@
-/* Add JavaScript code here! */
-import React from 'react';
-import ReactDOM from 'react-dom';
-ReactDOM.render(<div>"HELLO REACT"</div>, document.getElementById('root'));
-
-//console.log('Hello World! You did! Welcome to Snowpack :D');
